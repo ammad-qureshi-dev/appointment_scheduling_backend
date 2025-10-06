3fd63465ffc48eb23073568937fbbb1fda1c4106
ammad-qureshi-dev <ammadq87@my.yorku.ca>
2025-10-05 23:57:38 -0400

    refactor: spotless

--- src/main/java/com/booker_app/backend_service/controllers/response/dto/UserDTO.java
+++ src/main/java/com/booker_app/backend_service/controllers/response/dto/UserDTO.java
@@ -1,19 +1,21 @@
+/* (C) 2025 
+Booker App. */
 package com.booker_app.backend_service.controllers.response.dto;
+
+import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-    private String fullName;
-    private String email;
-    private String phoneNumber;
-    private UUID userId;
+	private String fullName;
+	private String email;
+	private String phoneNumber;
+	private UUID userId;
 }
--- src/main/java/com/booker_app/backend_service/services/UserService.java
+++ src/main/java/com/booker_app/backend_service/services/UserService.java
@@ -2,26 +2,20 @@
 Booker App. */
 package com.booker_app.backend_service.services;
 
-import com.booker_app.backend_service.configs.JwtConfiguration;
+import java.util.UUID;
+
 import com.booker_app.backend_service.controllers.response.ResponseType;
 import com.booker_app.backend_service.controllers.response.dto.UserDTO;
 import com.booker_app.backend_service.exceptions.ServiceResponseException;
-import com.booker_app.backend_service.models.User;
 import com.booker_app.backend_service.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
-	private final JwtConfiguration jwtConfiguration;
-
-	public UserService(UserRepository userRepository, JwtConfiguration jwtConfiguration) {
-		this.userRepository = userRepository;
-		this.jwtConfiguration = jwtConfiguration;
-	}
 
 	public UserDTO getUserById(UUID userId) {
 		var userOpt = userRepository.getUserById(userId);
@@ -30,12 +24,8 @@ public class UserService {
 		}
 
 		var user = userOpt.get();
-		return UserDTO.builder()
-				.email(user.getEmail())
-				.fullName(user.getFullName())
-				.phoneNumber(user.getPhoneNumber())
-				.userId(userId)
-				.build();
+		return UserDTO.builder().email(user.getEmail()).fullName(user.getFullName()).phoneNumber(user.getPhoneNumber())
+				.userId(userId).build();
 	}
 
 }
